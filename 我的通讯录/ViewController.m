165a44ccad609369bf07fa92d5efc805ffe9b8db
xh <xh@xhdemac-mini.local>
2016-05-20 21:03:44 +0800

    Initial Commit

---
+++ 我的通讯录/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  我的通讯录
+//
+//  Created by xh on 16/5/20.
+//  Copyright © 2016年 xh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 我的通讯录/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  我的通讯录
+//
+//  Created by xh on 16/5/20.
+//  Copyright © 2016年 xh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
